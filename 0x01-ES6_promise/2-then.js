e3777e7285f98bd581c8be173db1edbd791c5cca
FahdAzouz <fahdstudies00@gmail.com>
2024-05-29 18:13:53 +0100

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,7 @@
 /* eslint-disable */
 export default function getResponseFromAPI(success) {
-  const arr = new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({ status: 200, body: "Success" });
-    }
-    else{
-      reject(new Error("The fake API is not working currently"))
-    }
-  });
-  console.log("Got a response from the API");
-  return (arr);
+  return promise
+    .then(() => ({ status: 200, body: "Success"}))
+    .catch(() => Error())
+    .finally(() => {console.log("Got a response from the API"); });
 }
